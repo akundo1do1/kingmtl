81995a6bcb1d57fa6a6065dfbad4dd63f0586e03
akundo1do1 <akundo1do1@gmail.com>
2024-10-03 20:55:49 +0700

    Add files via upload

---
+++ appConfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: "KingMTL.com",
+  tagline: "KingMTL Contents Article & Image",
+  description: "KingMTL Contents Article & Image",
+  keywords: "cdom,cdom agc,cdom blog",
+  author: "KingMTL.com",
+  favicon: "https://pugjs.org/images/favicon.ico",
+  imageCount: 30,
+  removeBadWords: "porn,nude,sex",
+};
